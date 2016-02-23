c17047fe1a5934a2f2f690d282edb20a31bc1285
Loïc <loic.porte@corp.ovh.com>
2016-02-23 13:52:47 +0100

    List checks command

--- sauna/main.py
+++ sauna/main.py
@@ -2,15 +2,16 @@
 """Health checker
 
 Usage:
-  main.py sample
-  main.py [--level=<lvl>] [--config=FILE]
-  main.py (-h | --help)
-  main.py --version
+  sauna [--level=<lvl>] [--config=FILE]
+  sauna sample
+  sauna list-checks [--config=FILE]
+  sauna (-h | --help)
+  sauna --version
 
 Options:
   -h --help      Show this screen.
   --version      Show version.
-  --level=<lvl>  Log level [default: info].
+  --level=<lvl>  Log level [default: warn].
   --config=FILE  Config file [default: sauna.yml].
 
 """
@@ -27,14 +28,16 @@ def main():
     logging.basicConfig(
         format='%(asctime)s - %(levelname)-8s - %(message)s',
         datefmt='%Y/%m/%d %H:%M:%S',
-        level=getattr(logging, args['--level'].upper(), 'INFO')
+        level=getattr(logging, args['--level'].upper(), 'WARN')
     )
     logging.getLogger('requests').setLevel(logging.ERROR)
 
     if args.get('sample'):
         file_path = sauna.assemble_config_sample('./')
         print('Created file {}'.format(file_path))
-
+    elif args.get('list-checks'):
+        for name in sauna.get_checks_name(conf_file):
+            print('{}'.format(name))
     else:
         sauna.launch(conf_file)
 
