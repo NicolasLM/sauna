3e5c23a72ee738bf57da75c1887cc1b08b2a13ab
Nicolas Le Manchet <nicolas.le-manchet@ovh.net>
2016-04-29 16:09:03 +0200

    Bump version to 0.0.5

--- sauna/__init__.py
+++ sauna/__init__.py
@@ -18,7 +18,7 @@ from sauna.consumers.base import QueuedConsumer
 from sauna.consumers import ConsumerRegister
 from sauna.plugins import PluginRegister
 
-__version__ = '0.0.4'
+__version__ = '0.0.5'
 
 ServiceCheck = namedtuple('ServiceCheck',
                           ['timestamp', 'hostname', 'name',
